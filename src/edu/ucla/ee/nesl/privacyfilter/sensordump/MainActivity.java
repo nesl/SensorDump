7c685ec7afe71f8abd410c9dc94907f4929a9fda
Chenguang Shen <pscggeforce@gmail.com>
2013-09-15 21:14:07 -0700

    remove ui part

--- src/edu/ucla/ee/nesl/privacyfilter/sensordump/MainActivity.java
+++ src/edu/ucla/ee/nesl/privacyfilter/sensordump/MainActivity.java
@@ -1,140 +1,82 @@
 package edu.ucla.ee.nesl.privacyfilter.sensordump;
 
-import android.os.Bundle;
-import android.content.Context;
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.view.LayoutInflater;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorEvent;
-
-import java.util.ArrayList;
 
 public class MainActivity extends Activity {
-
-	private static final String newline = System.getProperty("line.separator");
-
-	private static final int SENSORS_PER_PAGE = 3; // number of sensors to display at one time
+	private static final String TAG = "Activity";
+	private static HashSet<Integer> pool;
 
 	private class SensorDisplayer implements SensorEventListener { // {{{
-		private Context context;
+		private MainActivity parent;
 		private Sensor sensor;
-		private View rootView;
-		private TextView nameView;
-		private TextView accuracyView;
-		private TextView valuesView;
+		private long count;
+		private long start;
 		
-		public SensorDisplayer (Context baseContext, Sensor baseSensor, View baseRootView) {
-			this.context = baseContext;
+		public SensorDisplayer (MainActivity act, Sensor baseSensor) {
+			parent = act;
 			this.sensor = baseSensor;
-			this.rootView = baseRootView;
-			this.nameView = (TextView) this.rootView.findViewById(R.id.sensor_name);
-			this.accuracyView = (TextView) this.rootView.findViewById(R.id.sensor_accuracy);
-			this.valuesView = (TextView) this.rootView.findViewById(R.id.sensor_values);
-
-			nameView.setText(sensor.getVendor() + " " + sensor.getName() + newline + "(ver. " + Integer.toString(sensor.getVersion()) + ", resolution: " + Float.toString(sensor.getResolution()) + ")");
+			count = 0;
+			start = System.nanoTime();
 		}
 
 		public void onAccuracyChanged(Sensor sensor, int accuracy) {
-			accuracyView.setText("Accuracy: " + Integer.toString(accuracy));
 		}
 
 		public void onSensorChanged(SensorEvent event) {
-			StringBuilder values = new StringBuilder("Values: " + newline + "\t");
-			for (int idx = 0; idx < event.values.length; idx++) {
-				values.append(Float.toString(event.values[idx]));
-
-				if (idx !=event.values.length - 1) {
-					values.append(newline + "\t");
-				}
+//			if (event.values[0] == -this.sensor.getType()) {
+//				Log.d(TAG, "constant works for " + this.sensor.getType());
+//			}
+			count++;
+			//Log.d(TAG, "for sensor " + sensor.getName() + " count=" + count);
+			if (count >= 10000) {
+				long now = System.nanoTime();
+				Log.d(TAG, "time to get 10000 values for Sensor " + sensor.getName() + "=" + ((double)(now - start) / 1000000000.0));
+				parent.sensorManager.unregisterListener(this);
+				count = 0;
 			}
-
-			valuesView.setText(values.toString());
 		}
 
-		public void removeView () {
-			((ViewGroup) rootView.getParent()).removeView(rootView);
-		}
 	} // }}}
-	private class NavButtonListener implements OnClickListener {
-		public void onClick(View button) {
-			if (button.getId() == R.id.prevButton) {
-				if (currentlyShowingFrom >= SENSORS_PER_PAGE) {
-					currentlyShowingFrom -= SENSORS_PER_PAGE;
-					updateDisplay();
-				}
-			} else if (button.getId() == R.id.nextButton) {
-				if (currentlyShowingFrom < allSensors.size() - SENSORS_PER_PAGE) {
-					currentlyShowingFrom += SENSORS_PER_PAGE;
-					updateDisplay();
-				}
-			}
-		}
-	}
-
-	private ViewGroup contentBodyView;
-	private LayoutInflater inflater;
+	
 	private SensorManager sensorManager;
-
 	private ArrayList<Sensor> allSensors;
-
-	private int currentlyShowingFrom = 0;
-	private ArrayList<Sensor> displayedSensors;
-	private ArrayList<SensorDisplayer> sensorDisplayers; // parallel with displayedSensors
 
 	// update the display, showing a page-worth of sensors, starting at the index given by currentlyShowingFrom
 	private void updateDisplay() {
-		// stop displaying the old page of sensors
-		for (int sensorIdx = 0; sensorIdx < displayedSensors.size(); sensorIdx++) {
-			sensorDisplayers.get(sensorIdx).removeView();
-			sensorManager.unregisterListener(sensorDisplayers.get(sensorIdx), displayedSensors.get(sensorIdx));
-		}
-
-		// figure out which sensors to display now
-		int showTo = Math.min(currentlyShowingFrom + SENSORS_PER_PAGE, allSensors.size());
-
-		// start displaying the new page of sensors
-		displayedSensors = new ArrayList<Sensor>(allSensors.subList(currentlyShowingFrom, showTo));
-		sensorDisplayers = new ArrayList<SensorDisplayer>(); // parallel array
-		for (int sensorIdx = 0; sensorIdx < displayedSensors.size(); sensorIdx++) {
-			Sensor curSensor = displayedSensors.get(sensorIdx);
-			View curSensorView = inflater.inflate(R.layout.sensor, null);
-			SensorDisplayer sd = new SensorDisplayer(this, curSensor, curSensorView);
-			sensorDisplayers.add(sensorIdx, sd);
-			contentBodyView.addView(curSensorView);
-
-			sensorManager.registerListener(sd, curSensor, SensorManager.SENSOR_DELAY_NORMAL);
+		// start displaying a page of sensors
+		for (Sensor s:allSensors) {
+			if (!pool.contains(s.getType())) {
+				SensorDisplayer sd = new SensorDisplayer(this, s);
+				sensorManager.registerListener(sd, s, SensorManager.SENSOR_DELAY_FASTEST);
+				//Log.d(TAG, "sensor name=" + s.getName() + ", sensor type=" + s.getType());
+				pool.add(s.getType());
+			}
 		}
 	}
 
 	private void initializeData () {
-		contentBodyView = (ViewGroup) findViewById(R.id.content);
-		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
 		allSensors = new ArrayList<Sensor>(sensorManager.getSensorList(Sensor.TYPE_ALL));
-
-		displayedSensors = new ArrayList<Sensor>();
-		sensorDisplayers = new ArrayList<SensorDisplayer>(); // parallel with displayedSensors
+		Log.d(TAG, "sensor size=" + allSensors.size());
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		initializeData();
-
-		findViewById(R.id.prevButton).setOnClickListener(new NavButtonListener());
-		findViewById(R.id.nextButton).setOnClickListener(new NavButtonListener());
-		
-		updateDisplay();
+		pool = new HashSet<Integer>();
 	}
 
 
@@ -143,6 +85,13 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void onBtnClicked(View v){
+		if (v.getId() == R.id.button1) {
+			initializeData();		
+			updateDisplay();
+		}
 	}
 	
 }
